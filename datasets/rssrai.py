c943baf2d163edda1384fc1203250b9bd9243c84
Gummary <sdythp@gmail.com>
2019-07-10 08:51:56 +0800

    add rssrai dataset

---
+++ datasets/rssrai.py
@@ -0,0 +1,17 @@
+
+import logging
+
+import torch
+from torch.utils.data import Dataset
+logger = logging.getLogger(__name__)
+
+class RssraiDataset(Dataset):
+
+    def __init__():
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem()__(self):
+        pass
